c5b5074450934b5a35d8975211fc0a408d92dc4e
ushakovaalyona <alyona_ushakova_02@mail.ru>
2021-09-12 14:32:22 +0500

    Задание 2
    Добавлен вывод произведения и частного введенных чисел

--- Source1.cpp
+++ Source1.cpp
@@ -8,7 +8,11 @@ int main() {
     cout << "Сумма чисел равна " << a + b << endl;
     cout << "Разнсть чисел равна " << abs(a - b) << endl;
     cout << "Среднее арифметическое модулей этих чисел равно " << (abs(a) + abs(b)) / 2 << endl;
-
+    cout << "Произведение чисел равно " << a*b << endl;
+    int ch;
+    if (a > b) { ch = a / b; }
+    else { ch = b / a; }
+    cout << "Частное чисел равно " << ch << endl;
 
     return 0;
-}S
+}
